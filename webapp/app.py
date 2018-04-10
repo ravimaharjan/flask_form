179b456f4379d5afc22896de722cb15c33a1bab6
Ravi Maharjan <ravi_maharjan@logpoint.com>
2018-04-10 19:44:26 +0545

    First template example

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -7,8 +7,20 @@ app.debug = True
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello Flask"
+    user = {'username': 'FlaskUser'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
 
